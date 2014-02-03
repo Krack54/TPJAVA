44cc168a7ae94b01c89454184efccb58b4481b27
Robrock <robin.kebaili@gmail.com>
2014-02-03 15:36:05 +0100

    123456

--- src/server/DispatcherInterface.java
+++ src/server/DispatcherInterface.java
@@ -1,7 +1,12 @@
 package server;
 
 import java.rmi.Remote;
+import client.ReceiveInterface;
 
 public interface DispatcherInterface extends Remote {
+	
+	public abstract void inscription(ReceiveInterface client);
+	
+	public abstract void DispatchMessage(String s);
 
 }
