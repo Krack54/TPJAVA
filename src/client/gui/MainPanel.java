6059e090d19dd9510e7c452387db997cb800cab6
Robrock <robin.kebaili@gmail.com>
2014-02-17 15:38:56 +0100

    Ajout du dessin basique
    (Note : bug de la ligne fragmentée apparu)

--- src/client/gui/MainPanel.java
+++ src/client/gui/MainPanel.java
@@ -6,6 +6,7 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
@@ -14,20 +15,25 @@ public class MainPanel extends JPanel {
 	// Fields
 	private int mouseX,mouseY;
 	private static int PEN_SIZE = 10;
+	private BufferedImage image;
+	public final static int PANEL_WIDTH = 800;
+	public final static int PANEL_HEIGHT = 600;
+	
 	
 	// Constructors
 	public MainPanel(){
 		this.mouseX = 200;
 		this.mouseY = 200;
 		
-		this.setPreferredSize(new Dimension(800,600));
+		this.image = new BufferedImage(MainPanel.PANEL_WIDTH,MainPanel.PANEL_HEIGHT,BufferedImage.TYPE_INT_ARGB);
+		
+		this.setPreferredSize(new Dimension(MainPanel.PANEL_WIDTH,MainPanel.PANEL_HEIGHT));
 		this.addMouseListener(new MouseListener() {
 			public void mouseReleased(MouseEvent arg0) {
 				
 			}
 			
 			public void mousePressed(MouseEvent arg0) {
-				
 			}
 			
 			public void mouseExited(MouseEvent arg0) {
@@ -55,7 +61,10 @@ public class MainPanel extends JPanel {
 				MainPanel.this.mouseY = arg0.getY();
 				MainPanel.this.repaint();
 				
-				// TODO dessin
+
+				Graphics g = MainPanel.this.image.getGraphics();
+				g.setColor(Color.black);
+				g.fillOval(arg0.getX()-MainPanel.PEN_SIZE/2,arg0.getY()-MainPanel.PEN_SIZE/2,MainPanel.PEN_SIZE,MainPanel.PEN_SIZE);
 			}
 		});
 	}
@@ -63,7 +72,8 @@ public class MainPanel extends JPanel {
 	// Methods
 	public void paintComponent(Graphics g){
 		g.setColor(Color.white);
-		g.fillRect(0, 0, this.getWidth(),this.getHeight());
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.drawImage(this.image, 0, 0, null);
 		
 		g.setColor(Color.black);
 		g.fillOval(this.mouseX-MainPanel.PEN_SIZE/2,this.mouseY-MainPanel.PEN_SIZE/2,MainPanel.PEN_SIZE,MainPanel.PEN_SIZE);
